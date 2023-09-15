9300cd5955452224bf300e02eee3926a234365e5
Davi Alves <cadwvk@gmail.com>
2023-09-15 20:54:42 -0300

    Created generate-ai-completion POST route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { createTranscriptionRoute } from './routes/create-transcription'
+import { generateAICompletionRoute } from './routes/generate-ai-completion'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
 
@@ -8,6 +9,7 @@ const app = fastify()
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
 app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app
   .listen({
